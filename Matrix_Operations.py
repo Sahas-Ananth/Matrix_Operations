ea2b5bb71566d1ce33dbb5923f52a54dad768b45
Sahas-Ananth <sahas_ananth@outlook.com>
2020-05-12 15:34:32 +0530

    Final commit with terminal printing

--- Matrix_Operations.py
+++ Matrix_Operations.py
@@ -1,47 +1,55 @@
 import numpy as np
 from sympy import *
 
+# 0,1,0;0,0,1;-6,-11,-6
+
 
 class Matrix_Operations:
     def __init__(self):
-        self.s = symbols("S")
+        self.s = symbols("lamda")
         init_printing(pretty_print=True)
+        print("\n\nThe following rules must be followed to while entering the values of the matrix:")
+        print("=> Add a \",\" after each elemnet in a row.\n=> After each row add a \";\". \n=> Write \"None\" if you don't have the matrix")
+        print("Example: 1,2,-3;4,-5,6;-7,8,9")
 
-    def __get_input(self):
+    def __get_input(self, Name):
         """Gets input from user as a string and generates a matrix.
 
         Returns:
             Matrix -- Sympy Matrix
         """
-        print("\n\nThe following rules must be followed to while entering the values of the matrix:")
-        print("=> Add a \",\" after each elemnet in a row.\n=> After each row add a \";\".")
-        print("Example: 1,2,-3;4,-5,6;-7,8,9")
+        print("\n\nNow, you are about to enter %s Matrix" % Name)
         str = input("Enter the matrix here: ")
         push = []
-        for row in str.split(";"):
-            dummy = []
-            for number in row.split(","):
-                try:
-                    number = int(number)
-                except:
-                    number = float(number)
-                dummy.append(number)
-            push.append(dummy)
+        if str == "None":
+            pass
+        else:
+            for row in str.split(";"):
+                dummy = []
+                for number in row.split(","):
+                    try:
+                        number = int(number)
+                    except:
+                        number = float(number)
+                    dummy.append(number)
+                push.append(dummy)
         Mat = Matrix(push)
+        print("\nThe %s Matrix you have entered is: " % Name)
+        pprint(Mat)
         return Mat
 
     def __si_am(self, Mat_A):
         n, m = Mat_A.shape
         Mat = ((self.s*eye(n)) -
                Mat_A) if n == m else print("Error!: ", ShapeError(NonSquareMatrixError))
-        print("λI-A matrix:")
+        print("\nλI-A matrix:")
         pprint(Mat)
         return Mat
 
     def __char_eq(self, Mat):
         eqn = expand(Mat.det())
         print("\nThe Characteristic equation is: ")
-        print(eqn)
+        pprint(eqn)
         return eqn
 
     def __roots(self, eqn):
@@ -52,32 +60,52 @@ class Matrix_Operations:
         return sols
 
     def __Cofactor_Matrix(self, Mat):
-        Cf_mat = expand(Mat.cofactor_matrix())
-        print("\nCoFactor Matrix: ")
-        pprint(Cf_mat.row(0))
-        return Cf_mat.row(0)
+        Cf_mat = Mat.cofactor_matrix()
+        print("\n\nCoFactor Matrix: ")
+        cf = Cf_mat.row(0).T
+        pprint(expand(cf))
+        return cf
 
     def __ModalMatrix(self, Mat, sols):
         mm = Matrix()
         for key, value in sols.items():
             dummy = Matrix()
             while value > 0:
-                f = lambdify(self.s, Derivative(Mat, self.s, value-1,
-                                                evaluate=True), "numpy")
-                dummy = dummy.row_insert(value-1, Matrix(f(key)))
+                di = Derivative(Mat, self.s, value-1, evaluate=True)
+                f = lambdify(self.s, di, "numpy")
+                dummy = dummy.col_insert(
+                    value-1, Matrix((f(key)/np.gcd.reduce(f(key)))))
                 value -= 1
-            mm = mm.col_join(dummy)
-        mm = mm.T
+            mm = mm.row_join(dummy)
+        print("\nModal Matrix:")
         pprint(mm)
+        print("\n Inverse Modal Matrix:")
+        pprint(mm**-1)
         return mm
 
+    def __finalStep(self, M, Amat, Bmat, Cmat):
+        A_bar = (M**-1)*Amat*M
+        B_bar = (M**-1)*Bmat
+        C_bar = Cmat*M
+        print("\nNew A Matrix:")
+        pprint(A_bar)
+        print("\nNew B Matrix:")
+        pprint(B_bar)
+        print("\nNew C Matrix:")
+        pprint(C_bar)
+        return A_bar, B_bar, C_bar
+
     def main(self):
-        A = self.__get_input()
+        A = self.__get_input("A")
+        B = self.__get_input("B")
+        C = self.__get_input("C")
+        # D = self.__get_input("D")
         si_am = self.__si_am(A)
         char_eq = self.__char_eq(si_am)
         roots = self.__roots(char_eq)
         co_fac_row = self.__Cofactor_Matrix(si_am)
         mm = self.__ModalMatrix(co_fac_row, roots)
+        Na, Nb, Nc = self.__finalStep(mm, A, B.T, C)
 
 
 if __name__ == "__main__":
